a3df8f554c66a7b8abbefbc6cea9ea2c81233bc7
Isopodus <isopodus@gmail.com>
2021-01-20 19:07:01 +0200

    Added RN location

--- android/app/src/main/java/com/arnavi/MainApplication.java
+++ android/app/src/main/java/com/arnavi/MainApplication.java
@@ -3,6 +3,7 @@ package com.arnavi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.reactnativecommunity.location.RNLocationPackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLocationPackage(),
             new RNScreensPackage(),
             new SafeAreaContextPackage(),
             new RNGestureHandlerPackage(),
