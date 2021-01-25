3d8e4e37caad4ac067df03049f84adc407f05c4a
NevedomaZverushka <lunna.savovich@gmail.com>
2021-01-26 00:25:40 +0200

    Minor splashscreen improvements

--- src/Navigator.js
+++ src/Navigator.js
@@ -5,16 +5,18 @@ import Splash from './screens/Splash';
 import FavoriteLocations from './screens/FavoriteLocations';
 import ARNavigator from "./screens/ARNavigator";
 import {createStackNavigator} from "@react-navigation/stack";
+import {useSelector} from "react-redux";
 
 const Stack = createStackNavigator();
 
 const arnavi = () => <ARNavigator/>;
 
 export default function Navigator() {
+    const status = useSelector(state => state.appReady);
     return(
         <NavigationContainer>
             <Stack.Navigator initialRouteName={"Splash"}>
-                <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />
+                {!status && <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}} />}
                 <Stack.Screen name="Home" component={Home} options={{headerShown: false}} />
                 <Stack.Screen name="FavoriteLocations" component={FavoriteLocations} options={{headerShown: false}} />
                 <Stack.Screen name="AR" component={arnavi} options={{headerShown: false}} />
