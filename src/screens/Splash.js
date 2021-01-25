d75e66d0bdcead21f64bbd1ca5181288f70b5023
NevedomaZverushka <lunna.savovich@gmail.com>
2021-01-25 23:23:45 +0200

    Updated splash screen

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -3,16 +3,18 @@ import LottieView from 'lottie-react-native';
 import spinner from '../assets/animations/spinner';
 import {useSelector} from "react-redux";
 import {useNavigation} from "@react-navigation/native";
+import getTheme from "../global/Style";
 
 export default function Splash() {
     const status = useSelector(state => state.appReady);
     const { navigate } = useNavigation();
+    const theme = getTheme();
 
     React.useEffect(() => {
         if (status) navigate('Home');
     }, [status]);
 
     return(
-        <LottieView source={spinner} autoPlay loop />
+        <LottieView style={{ backgroundColor: theme.black }}  source={spinner} autoPlay loop />
     )
 }
